cad52a0a5e73c38d29f593c36bf3fec670b9e764
dasdasd123z <46165713+dasdasd123z@users.noreply.github.com>
2018-12-26 19:29:41 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,12 +2,3 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GiftSel.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "django not imported"
-        ) from exc
-    execute_from_command_line(sys.argv)
