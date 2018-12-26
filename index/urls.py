a8de358785f1bded2db8da41550c2340c77a808e
xarizma <xarizma@protonmail.com>
2018-12-26 19:16:06 +0300

    index/ => /
    fix url's

--- index/urls.py
+++ index/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'select/$', views.select, name="select")
+    path('', views.index, name="index"),
+    path('select/', views.select, name="select")
 ]
